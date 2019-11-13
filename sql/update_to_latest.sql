7e625483ae76496b69c773ad343d4f03b3a43096
Charles Stein <charles.mst@gmail.com>
2019-11-13 02:28:20 -0300

    database migration

--- sql/update_to_latest.sql
+++ sql/update_to_latest.sql
@@ -1,4 +1,6 @@
-﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+﻿info: Microsoft.EntityFrameworkCore.Infrastructure[10403]
+      Entity Framework Core 2.2.6-servicing-10079 initialized 'BraviApiDbContext' using provider 'Microsoft.EntityFrameworkCore.SqlServer' with options: None
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
 BEGIN
     CREATE TABLE [__EFMigrationsHistory] (
         [MigrationId] nvarchar(150) NOT NULL,
@@ -9,44 +11,48 @@ END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20191111194809_InitialCreate')
-BEGIN
-    CREATE TABLE [People] (
-        [Id] uniqueidentifier NOT NULL,
-        [Name] nvarchar(500) NOT NULL,
-        [BirthDate] datetime2 NOT NULL,
-        CONSTRAINT [PK_People] PRIMARY KEY ([Id])
-    );
-END;
+CREATE TABLE [People] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(500) NOT NULL,
+    [BirthDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_People] PRIMARY KEY ([Id])
+);
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20191111194809_InitialCreate')
-BEGIN
-    CREATE TABLE [Contacts] (
-        [Id] uniqueidentifier NOT NULL,
-        [PersonId] uniqueidentifier NOT NULL,
-        [Value] nvarchar(max) NULL,
-        [Type] int NOT NULL,
-        CONSTRAINT [PK_Contacts] PRIMARY KEY ([Id]),
-        CONSTRAINT [FK_Contacts_People_PersonId] FOREIGN KEY ([PersonId]) REFERENCES [People] ([Id]) ON DELETE CASCADE
-    );
-END;
+CREATE TABLE [Contacts] (
+    [Id] uniqueidentifier NOT NULL,
+    [PersonId] uniqueidentifier NOT NULL,
+    [Value] nvarchar(max) NULL,
+    [Type] int NOT NULL,
+    CONSTRAINT [PK_Contacts] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Contacts_People_PersonId] FOREIGN KEY ([PersonId]) REFERENCES [People] ([Id]) ON DELETE CASCADE
+);
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20191111194809_InitialCreate')
-BEGIN
-    CREATE INDEX [IX_Contacts_PersonId] ON [Contacts] ([PersonId]);
-END;
+CREATE INDEX [IX_Contacts_PersonId] ON [Contacts] ([PersonId]);
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20191111194809_InitialCreate')
-BEGIN
-    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20191111194809_InitialCreate', N'2.2.6-servicing-10079');
-END;
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20191111194809_InitialCreate', N'2.2.6-servicing-10079');
 
 GO
+
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[People]') AND [c].[name] = N'BirthDate');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [People] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [People] DROP COLUMN [BirthDate];
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20191113052608_RemoveBirthdateFromPerson', N'2.2.6-servicing-10079');
+
+GO
+
 
